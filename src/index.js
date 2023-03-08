fa2f1f32d7f4c13cdafdfa484981a6a282d87521
Oleksandr <alex.vovchok@gmail.com>
2023-03-08 20:17:55 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from "axios";
+
+
+// Your API key: 34240691-69b0febad4566a0b07df5e473
+// https://pixabay.com/api/
